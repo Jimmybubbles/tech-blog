0e395fa9ffdf8642611d71f585e6fd3083ac05b8
james <james.anthony.russell36@gmail.com>
2022-12-04 15:01:00 +1100

    added main handlebars and updated server

--- server.js
+++ server.js
@@ -1,16 +1,28 @@
 const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
-
+const session = require('express-session')
 const routes = require('./controllers');
 const sequelize = require('.config/connection')
 const helpers = require('./utils/helpers');
-const { appendFile } = require('fs');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const hbs = exphbs.create({ helpers });
+
+const sess = {
+    secret: 'super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitalized: true,
+    store: new SequelizeStore ({
+        db: sequelize
+    })
+};
+
+app.use(session(sess));
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
